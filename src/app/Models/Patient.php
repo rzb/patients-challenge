9e3b32fa328753b82ddedc40cddb9aaa865d9d7a
rzb <renatozuma@gmail.com>
2023-03-30 22:55:37 -0300

    Implement models and migrations

---
+++ src/app/Models/Patient.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'mothers_name',
+        'birthdate',
+        'cpf',
+        'cns',
+    ];
+
+    protected $casts = [
+        'birthdate' => 'date:Y-m-d',
+    ];
+
+    public function address(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
+}
---
+++ src/database/migrations/2023_03_30_194728_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Patient;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignIdFor(Patient::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->char('cep', 8);
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->char('uf', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};

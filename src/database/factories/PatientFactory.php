7f578fa51d6ddc3718cfb4d037a1e2f03831931f
rzb <renatozuma@gmail.com>
2023-03-30 22:56:45 -0300

    Implement factories and seeder

---
+++ src/database/factories/PatientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'mothers_name' => $this->faker->name(gender: 'female'),
+            'birthdate' => $this->faker->date(),
+            'cpf' => $this->faker->cpf(formatted: false),
+            'cns' => $this->faker->numerify(str_repeat('#', 15)),
+        ];
+    }
+}

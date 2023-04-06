d4bea5d6f1315f25df0fc4df72eca5fb91870588
rzb <renatozuma@gmail.com>
2023-04-06 05:15:41 -0300

    Validate import file

--- src/app/Imports/PatientsImport.php
+++ src/app/Imports/PatientsImport.php
@@ -3,14 +3,19 @@
 namespace App\Imports;
 
 use App\Models\Patient;
+use App\Rules\Cep;
+use App\Rules\Cns;
+use App\Rules\Cpf;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Row;
 
-class PatientsImport implements OnEachRow, WithHeadingRow, WithChunkReading, ShouldQueue
+class PatientsImport implements OnEachRow, WithHeadingRow, WithChunkReading, ShouldQueue, WithValidation
 {
     use Importable;
 
@@ -41,5 +46,33 @@ class PatientsImport implements OnEachRow, WithHeadingRow, WithChunkReading, Sho
     public function chunkSize(): int
     {
         return 500;
+    }
+
+    public function prepareForValidation(array $row): array
+    {
+        return array_merge($row, [
+            'cep' => Str::removeNonDigits($row['cep']),
+            'cns' => Str::removeNonDigits($row['cns']),
+            'cpf' => Str::removeNonDigits($row['cpf']),
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'foto' => 'required',
+            'nome' => 'required',
+            'nome_da_mae' => 'required',
+            'data_de_nascimento' => 'required|date_format:Y-m-d',
+            'cpf' => ['required', new Cpf()],
+            'cns' => ['required', new Cns()],
+            'cep' => ['required', new Cep()],
+            'logradouro' => 'required',
+            'numero' => 'required',
+            'complemento' => 'nullable',
+            'bairro' => 'required',
+            'localidade' => 'required',
+            'uf' => 'required|size:2', // @todo consider enum or lookup table validation
+        ];
     }
 }

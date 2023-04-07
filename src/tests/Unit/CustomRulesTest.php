6b8a327f058f41848dbe6c30689690aa6f8e1d94
rzb <renatozuma@gmail.com>
2023-04-07 07:30:24 -0300

    Improve CNS test coverage

--- src/tests/Unit/CustomRulesTest.php
+++ src/tests/Unit/CustomRulesTest.php
@@ -22,65 +22,43 @@ class CustomRulesTest extends TestCase
     /**
      * @test
      * @dataProvider provideCpf
-     */
-    public function it_validates_cpf($value, $shouldFail, $message): void
-    {
-        $this->expectExceptionOnlyIf($shouldFail, $message);
-
-        (new Cpf())->validate('cpf', $value, $this->fail);
-    }
-
-    /**
-     * @test
      * @dataProvider provideCns
-     */
-    public function it_validates_cns($value, $shouldFail, $message): void
-    {
-        $this->expectExceptionOnlyIf($shouldFail, $message);
-
-        (new Cns())->validate('cns', $value, $this->fail);
-    }
-
-    /**
-     * @test
      * @dataProvider provideCep
      */
-    public function it_validates_cep($value, $shouldFail, $message): void
+    public function it_validates_a_field($rule, $value, $shouldFail, $message): void
     {
         $this->expectExceptionOnlyIf($shouldFail, $message);
 
-        (new Cep())->validate('cep', $value, $this->fail);
+        $rule->validate('whatever', $value, $this->fail);
     }
 
     private function provideCpf(): Generator
     {
-        yield 'valid with digits only' => ['20501792007', false, null];
+        yield 'valid number' => [new Cpf(), '20501792007', false, null];
 
-        yield 'invalid repeating digits' => ['99999999999', true, 'validation.cpf'];
+        yield 'invalid repeating digits' => [new Cpf(), '99999999999', true, 'validation.cpf'];
 
-        yield 'invalid check digit fails' => ['20501792008', true, 'validation.cpf'];
+        yield 'invalid check digit fails' => [new Cpf(), '20501792008', true, 'validation.cpf'];
 
-        yield 'invalid too short' => ['2050179200', true, 'validation.cpf'];
+        yield 'invalid too short' => [new Cpf(), '2050179200', true, 'validation.cpf'];
 
-        yield 'invalid too long' => ['205017920073', true, 'validation.cpf'];
+        yield 'invalid too long' => [new Cpf(), '205017920073', true, 'validation.cpf'];
     }
 
     private function provideCns(): Generator
     {
-        yield 'valid with digits only' => ['120413834000008', false, null];
+        yield 'valid number' => [new Cns(), '120413834000008', false, null];
 
-        yield 'invalid too short' => ['2050179200', true, 'validation.cns'];
-
-        yield 'invalid too long' => ['205017920073', true, 'validation.cns'];
+        yield 'invalid number' => [new Cns(), '183839901480018', true, 'validation.cns'];
     }
 
     private function provideCep(): Generator
     {
-        yield 'valid with digits only' => ['69440970', false, null];
+        yield 'valid number' => [new Cep(), '69440970', false, null];
 
-        yield 'invalid too short' => ['6944097', true, 'validation.cep'];
+        yield 'invalid too short' => [new Cep(), '6944097', true, 'validation.cep'];
 
-        yield 'invalid too long' => ['694409702', true, 'validation.cep'];
+        yield 'invalid too long' => [new Cep(), '694409702', true, 'validation.cep'];
     }
 
     protected function expectExceptionOnlyIf(bool $condition, ?string $message)
